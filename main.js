fa708f2270f118bb5df6141554c2f63dfb6d798c
Aarow <aarow0021@gmail.com>
2025-03-07 22:57:02 -0800

    Better physics

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ function getDistance([x1, y1], [x2, y2]) {
 
 //ball class
 class Ball {
-  constructor(x, y, velX, velY, [r, g, b], size) {
+  constructor(x, y, velX, velY, [r, g, b], radius, mass) {
     this.x = x;
     this.y = y;
     this.velX = velX;
@@ -33,62 +33,104 @@ class Ball {
     this.r = r;
     this.g = g;
     this.b = b;
-    this.size = size;
-    //this.momentum = size * (Math.abs(velX) + Math.abs(velY));
+    this.radius = radius;
+    this.mass = mass || this.getArea();
+    this.density = this.getArea() / this.mass;
+    this.momentumX = this.mass * velX;
+    this.momentumY = this.mass * velY;
+    this.calcVelocity();
+    this.calcRadius();
   }
 
   //draws
   draw() {
     ctx.beginPath();
     ctx.fillStyle = `rgb(${this.r}, ${this.g}, ${this.b}`;
-    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
     ctx.fill();
   }
 
+  getArea() {
+    return Math.PI * this.radius ** 2
+  }
+
+  //a = pi * r^2
+  //r = sqrt(a / pi)
+  calcRadius() {
+    let area = this.mass / this.density
+    this.radius = Math.sqrt(area / Math.PI)
+  }
+
+  //v = momentum / mass
+  calcVelocity() {
+    this.velX = this.momentumX / this.mass;
+    this.velY = this.momentumY / this.mass;
+  }
+
+  //fuses this ball with another. The largest one is kept
+  fuse(ball2) {
+    let biggerBall = ball2.radius > this.radius ? ball2 : this;
+    let smallerBall = ball2.radius < this.radius ? ball2 : this;
+    let massRatio = smallerBall.mass / biggerBall.mass;
+    //color
+    biggerBall.r = ((biggerBall.r + smallerBall.r) / 2 + random(0, 255)) / 2;
+    biggerBall.g = ((biggerBall.g + smallerBall.g) / 2  + random(0, 255)) / 2;
+    biggerBall.b = ((biggerBall.b + smallerBall.b) / 2  + random(0, 255)) / 2;
+
+    //momentum
+    biggerBall.momentumX += smallerBall.momentumX;
+    biggerBall.momentumY += smallerBall.momentumY;
+    //mass
+    biggerBall.mass += smallerBall.mass;
+    //radius
+    biggerBall.radius = (biggerBall.radius + smallerBall.radius * .9);
+
+    //Calculates new velocity and radius from new mass and momentum
+    biggerBall.calcVelocity();
+    biggerBall.calcRadius();
+
+    //removes the smaller ball
+    balls.splice(balls.indexOf(smallerBall), 1);
+  }
+
   //handles ball collision checking and actions
-collisionDetect() {
-  for (let j = 0; j < balls.length; j++) {
-    if (!(this === balls[j])) {
+  collisionDetect() {
+    for (let j = 0; j < balls.length; j++) {
+      if (!(this === balls[j])) {
 
-      const distance = getDistance([this.x, this.y], [balls[j].x, balls[j].y]);
+        const distance = getDistance([this.x, this.y], [balls[j].x, balls[j].y]);
 
-      //collision condition
-      if (distance < this.size + balls[j].size) {
-        let biggerBall = balls[j].size > this.size ? balls[j] : this;
-        let smallerBall = balls[j].size < this.size ? balls[j] : this;
-        biggerBall.r = (biggerBall.r + smallerBall.r) / 2;
-        biggerBall.g = (biggerBall.g + smallerBall.g) / 2;
-        biggerBall.b = (biggerBall.b + smallerBall.b) / 2;
-        biggerBall.velX = biggerBall.velX + (smallerBall.velX * (smallerBall.size / biggerBall.size));
-        biggerBall.velY = (biggerBall.velY + smallerBall.velY);
-        biggerBall.size = (biggerBall.size + smallerBall.size * .9);
-        balls.splice(balls.indexOf(smallerBall), 1);
+        //collision condition
+        if (distance < this.radius + balls[j].radius) {
+          this.fuse(balls[j])
+        }
       }
     }
   }
-}
 
   //calculates balls next position/vectors
   update() {
-    if ((this.x + this.size) >= width) {
-      this.velX = -(this.velX);
-      this.x = width - this.size;
+    if ((this.x + this.radius) >= width) {
+      this.momentumX = -(this.momentumX);
+      this.x = width - this.radius;
     }
 
-    if ((this.x - this.size) <= 0) {
-      this.velX = -(this.velX);
-      this.x = 0 + this.size;
+    if ((this.x - this.radius) <= 0) {
+      this.momentumX = -(this.momentumX);
+      this.x = 0 + this.radius;
     }
 
-    if ((this.y + this.size) >= height) {
-      this.velY = -(this.velY);
-      this.y = height - this.size;
+    if ((this.y + this.radius) >= height) {
+      this.momentumY = -(this.momentumY);
+      this.y = height - this.radius;
     }
 
-    if ((this.y - this.size) <= 0) {
-      this.velY = -(this.velY);
-      this.y = 0 + this.size;
+    if ((this.y - this.radius) <= 0) {
+      this.momentumY = -(this.momentumY);
+      this.y = 0 + this.radius;
     }
+
+    this.calcVelocity();
 
     this.x += this.velX;
     this.y += this.velY;
@@ -122,25 +164,25 @@ function clickHandler(e) {
   let mouseY = e.clientY;
   for (let j = 0; j < balls.length; j++) {
     let distance = getDistance([mouseX, mouseY], [balls[j].x, balls[j].y])
-    if (distance < this.size) {
+    if (distance < this.radius) {
       //Split
     }
   }
 }
 
 //populates screen with balls
-function addBalls(num, x, y) {
+function addBalls(num, size, x, y) {
   for (let i = 0; i < num; i++) {
-    let size = random(10,20);
+    let radius = size || random(10,30);
     let ball = new Ball(
       // ball position always drawn at least one ball width
       // away from the edge of the canvas, to avoid drawing errors
-      x || random(0 + size,width - size),
-      y || random(0 + size,height - size),
+      x || random(0 + radius,width - radius),
+      y || random(0 + radius,height - radius),
       random(-7,7),
       random(-7,7),
       [random(0,255), random(0,255), random(0,255)],
-      size
+      radius
     );
   
     balls.push(ball);
@@ -157,7 +199,7 @@ window.addEventListener('resize', () => {
 window.addEventListener('click', clickHandler)
 
 //initiates the balls
-addBalls(20);
+addBalls(10);
 
 //initiates the loop
 loop()
